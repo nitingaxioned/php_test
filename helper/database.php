18c9c0c4ec19e33944e225150855ad244aa32453
Nitin <niting@axioned.com>
2021-12-29 13:14:29 +0530

    database class ceated

---
+++ helper/database.php
@@ -0,0 +1,24 @@
+<?php
+
+class Database {
+	private $host;
+	private $dbusername;
+	private $dbpassword;
+	private $dbname;
+	private $dbms;
+	private $dbmsStr;
+	
+	protected function connect(){
+		$this->dbms = 'mysql';
+		$this->host = 'localhost';
+		$this->dbusername = 'root';
+		$this->dbpassword = '';
+		$this->dbname = 'blog_php';
+		$this->dbmsStr=$this->dbms.':host='.$this->host.';dbname='.$this->dbname;
+		try {
+			return new PDO($this->dbmsStr, $this->dbusername, $this->dbpassword);
+		} catch(Exception $e) {
+			die($e->getMessage());
+		}
+	}
+}
