63f4f3f6a388266d6d80f40d35f32fd945bddc52
Nitin <niting@axioned.com>
2021-12-29 10:34:33 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Home</title>
+  <link rel="shortcut icon" href="favicon.ico" />
+  <link rel="stylesheet" media="screen" href="assets/css/style.css">
+</head>
+
+<body>
+  <!--container start-->
+  <div class="container">
+    <!--header section start-->
+    <header>
+     
+    </header>
+    <!--header section end-->
+    <!--main section start-->
+    <main>
+      
+    </main>
+    <!--main section end-->
+
+    <!--footer section start-->
+    <footer>
+
+    </footer>
+    <!--footer section end-->
+  </div>
+  <!--container end-->
+  <script src="assets/js/script.js"></script>
+</body>
+</html>
